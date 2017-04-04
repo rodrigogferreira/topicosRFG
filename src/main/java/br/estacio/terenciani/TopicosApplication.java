3d0135011488499d6c2e70bfcebc8854d88be996
Marcelo Figueiredo Terenciani <Marcelo Figueiredo Terenciani>
2017-04-04 18:11:32 -0400

    configuração do projeto spring boot

---
+++ src/main/java/br/estacio/terenciani/TopicosApplication.java
@@ -0,0 +1,12 @@
+package br.estacio.terenciani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopicosApplication.class, args);
+	}
+}
---
+++ src/test/java/br/estacio/terenciani/TopicosApplicationTests.java
@@ -0,0 +1,16 @@
+package br.estacio.terenciani;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TopicosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
