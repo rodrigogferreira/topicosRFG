21b06bd4dcffccedacdf081c5c20a90ac3c36108
Marcelo <Marcelo@10.13.185.104>
2017-05-09 21:43:35 -0400

    Metodo excluir

--- src/main/java/br/estacio/terenciani/controller/AlunoController.java
+++ src/main/java/br/estacio/terenciani/controller/AlunoController.java
@@ -3,7 +3,9 @@ package br.estacio.terenciani.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -23,4 +25,31 @@ public class AlunoController {
 		return this.alunoRepository.findAll();
 
 	}
+	
+	@RequestMapping(value="/aluno/{id}", 
+			method=RequestMethod.DELETE)
+	public HttpStatus excluir(@PathVariable Long id){
+		try{
+			this.alunoRepository.delete(id);
+			return HttpStatus.OK;
+		} catch (Exception e) {
+			return HttpStatus.INTERNAL_SERVER_ERROR;
+		}
+	}
+	/*
+	@RequestMapping(value="/aluno", 
+			method=RequestMethod.POST)
+	public void adicionar(Aluno aluno){
+		this.alunoRepository.save(aluno);
+	}
+	@RequestMapping(value="/aluno", 
+			method=RequestMethod.GET)
+	public void listarPorId(Long id){
+		this.alunoRepository.findOne(id);
+	}
+	@RequestMapping(value="/aluno", 
+			method=RequestMethod.PUT)
+	public void atualizar(Aluno aluno){
+		this.alunoRepository.save(aluno);
+	}*/
 }
